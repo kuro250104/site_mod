80b4f3d8cb3dba37cd046a2a2a5be4c410b299a5
Kuro <gaetan.bougoula@orange.fr>
2024-03-18 16:51:48 +0100

    V1.6.7 - Add delete hours  for CP/AM

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -7,10 +7,22 @@
             <h1 class="h3 mb-0 text-gray-800">Bienvenue sur la page d'accueil</h1>
         </div>
 
-
         <div class="row">
 
+
+
             <div class="col-lg-6">
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-success">Notes de mise à jour</h6>
+                    </div>
+                    <div class="card-body">
+                        <H5>Mise à jour des permissions CP/AM</H5>
+                        <p>- Les agents de maîtrise et les chefs de poste pourront désormais corriger et supprimer les heures que les opérateurs rentrent s'il y a des fautes</p>
+                    </div>
+
+                </div>
+
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
                         <h6 class="m-0 font-weight-bold text-success">Heures validées</h6>
