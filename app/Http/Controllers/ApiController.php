73467210be6ab97b0f72f31f945e63dcdba02f8e
Bougoula <gaetan.bougoula@zambongroup.com>
2024-05-02 08:46:46 +0200

    V2.1 - API Correcting

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,6 +6,7 @@ use App\Models\Hour;
 use App\Models\Team;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ApiController extends Controller
 {
@@ -14,5 +15,11 @@ class ApiController extends Controller
         $teams = Team::all();
         $hours = Hour::all();
         $users = User::all();
+
+        return response()->json([
+            'teams' => $teams,
+            'hours' => $hours,
+            'users' => $users
+        ], Response::HTTP_OK);
     }
 }
